b53bf850c845e0780594d42b7b535cffc8d77a39
NomotoEriko <nomoto.eriko@ist.osaka-u.ac.jp>
2019-02-05 21:45:38 +0900

    vis test

---
+++ jobs/runvistest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PYTHONPATH=$PYTHONPATH:/workspace/code
+python get_topics.py --coco_images data/coco\
+ --output data/coco/visual_concepts/image_topics_test.json\
+ --vocab_file /workspace/code/vocabs/vocab_train.pkl\
+ --prototxt_deploy ./visual_concepts_model/vgg/mil_finetune.prototxt.deploy\
+ --model visual_concepts_model/vgg/snapshot_iter_240000.caffemodel --batch_size 24 --tgt test
